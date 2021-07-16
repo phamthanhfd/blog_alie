90ce18f66fcfc5c29801b153e6004fe116eaa267
Pham Thanh <phamthanhfd@gmail.com>
2021-07-16 15:24:07 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//Http logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+  
+                app.engine('.hbs', exphbs({ extname: '.hbs' }));
+
+//Routes init
+route(app);
+
+app.listen(port, () => {
+                 console.log('Example app listening at http://localhost:${port}'); //127.0.0.1 - localhost
+});
+
+// Ctrl + C to turn off server localhost
